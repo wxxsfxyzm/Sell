47a652097ad225b82b8a1b42dd9b3845bcb5d1fa
Carl Yu <wxxsfxyzm@126.com>
2021-08-13 23:11:57 +0800

    Added LoginInterceptor for authentication, yet to be implemented for only admin pages need to be blocked, but now blocked once for all with several exclusions for dev purposes only.

---
+++ src/main/java/com/carlyu/configuration/LoginConfig.java
@@ -0,0 +1,37 @@
+package com.carlyu.configuration;
+
+import com.carlyu.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoginConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /*静态资源的位置*/
+        registry.addResourceHandler("/**/*").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**/*").addResourceLocations("classpath:/templates/");
+        /*放行swagger*/
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        String[] excludes = new String[]{
+                "/",
+                "/error",
+                "/admin/*",
+                "/api/*",
+                "/buyer/product/list"};
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(excludes);
+
+    }
+
+}
